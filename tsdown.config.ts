9cbe09977e21b225a8efb869b2d75bebcb7dc7ae
Michael Wang <michael19920327@gmail.com>
2025-06-07 03:26:01 +0800

    fix: remove astro from external

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -3,10 +3,4 @@ import { defineConfig } from 'tsdown';
 export default defineConfig(() => ({
   entry: ['src/cli/index.ts'],
   minify: true,
-  external: [
-    'astro',
-    '@astrojs/mdx',
-    '@astrojs/sitemap',
-    '@astrojs/rss',
-  ],
 }));
