1c3db810ed544b02159f877de7a8bb2b1707d9fa
Michael Wang <michael19920327@gmail.com>
2025-06-07 03:20:31 +0800

    fix: add astro to external

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -3,4 +3,10 @@ import { defineConfig } from 'tsdown';
 export default defineConfig(() => ({
   entry: ['src/cli/index.ts'],
   minify: true,
+  external: [
+    'astro',
+    '@astrojs/mdx',
+    '@astrojs/sitemap',
+    '@astrojs/rss',
+  ],
 }));
